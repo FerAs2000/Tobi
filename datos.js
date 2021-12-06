d3500b176b1baa90f1e8f582612b7d40b1e57be1
LicoresModelo <90228544+LicoresModelo@users.noreply.github.com>
2021-12-06 12:03:25 -0500

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,139 @@
+let products = [
+{
+	name: 'Poker',
+	tag:  'poker',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod0'
+},
+{
+	name: 'Club Colombia Dorada',
+	tag:  'ClubColombiaDorada',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod1'
+},
+{
+	name: 'Costeña',
+	tag:  'costena',
+	price: 2000,
+	inCart: 0,
+	ElemntoID: 'Prod2'
+	
+},
+{
+	name: 'Budweiser',
+	tag:  'budweiser',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod3'
+},
+{
+	name: 'Heineken',
+	tag:  'heineken',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod4'
+},
+{
+	name: 'Corona',
+	tag:  'Corona',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod5'
+},
+{
+	name: 'Ron Viejo De Caldas Media',
+	tag:  'RonViejoDeCaldasMedia',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod6'
+},
+{
+	name: 'Ron Viejo De Caldas Botella',
+	tag:  'RonViejoDeCaldasBotella',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod7'
+},
+{
+	name: 'Ron Viejo De Caldas Garrafa',
+	tag:  'RonViejoDeCaldasGarrafa',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod8'
+},
+{
+	name: 'Caucano Tradicional Media',
+	tag:  'AguardienteCaucanoMedia',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod9'
+},
+{
+	name: 'Caucano Tradicional Botella',
+	tag:  'AguardienteCaucanoBotella',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod10'
+},
+{
+	name: 'Caucano Sin Azúcar Botella',
+	tag:  'AguardienteCaucanoSinAzucarBotella',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod11'
+},
+{
+	name: 'Caucano Sin Azúcar Media',
+	tag:  'AguardienteCaucanoSinAzucarMedia',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod12'
+},
+{
+	name: 'Olmeca',
+	tag:  'Olmeca',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod13'
+},
+{
+	name: "Ballantine's",
+	tag:  'Ballantines',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod14'
+},
+{
+	name: "Jhonnie Walker Blenders' Batch",
+	tag:  'JhonnieWalkerBlendersBatch',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod15'
+},
+{
+	name: "Black And White",
+	tag:  'BlackAndWhite',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod16'
+},
+{
+	name: "Margarita Onduladas Familiar",
+	tag:  'MargaritaOnduladaFamiliar',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod17'
+},
+{
+	name: "Beck's",
+	tag:  'Becks',
+	price: 1000,
+	inCart: 0,
+	ElemntoID: 'Prod18'
+}
+];
+
+
+
